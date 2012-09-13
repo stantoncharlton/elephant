0525d642492efdc947fbb8753b362b6a830e92de
Ryan Dawson <ryan@thirteen23.com>
2012-09-13 15:08:26 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ElephantWebApp::Application.config.secret_token = '3e7d24c3f22a7231d6685f05722d4c98482b7d9e1d65d25e5a2bbc9d20ff4e2e32f136354338dfe4eba1d747f96623615696cc86afab05374c055c1218798809'
