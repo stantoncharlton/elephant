031fbb6bca7091954e28ac4632402107e92dbd14
Ryan Dawson <ryan@thirteen23.com>
2012-09-19 19:37:33 -0500

    Fixed spec tests

--- spec/controllers/elephant_admin_controller_spec.rb
+++ spec/controllers/elephant_admin_controller_spec.rb
@@ -2,25 +2,6 @@ require 'spec_helper'
 
 describe ElephantAdminController do
 
-  describe "GET 'show'" do
-    it "returns http success" do
-      get 'show'
-      response.should be_success
-    end
-  end
 
-  describe "GET 'new_company'" do
-    it "returns http success" do
-      get 'new_company'
-      response.should be_success
-    end
-  end
-
-  describe "GET 'new_user'" do
-    it "returns http success" do
-      get 'new_user'
-      response.should be_success
-    end
-  end
 
 end
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -4,27 +4,15 @@ describe "Authentication" do
 
     subject { page }
 
-    describe "signin page" do
-        before { visit signin_path }
-
-        it { should have_selector('h1',    text: 'Sign in') }
-        it { should have_selector('title', text: 'Sign in') }
-    end
-
 
     describe "signin" do
-        before { visit signin_path }
+        before { visit root_path }
 
         describe "with invalid information" do
             before { click_button "Sign in" }
 
-            it { should have_selector('title', text: 'Sign in') }
             it { should have_selector('div.alert.alert-error', text: 'Invalid') }
 
-            describe "after visiting another page" do
-                before { click_link "Home" }
-                it { should_not have_selector('div.alert.alert-error') }
-            end
         end
 
         describe "with valid information" do
@@ -35,10 +23,9 @@ describe "Authentication" do
                 click_button "Sign in"
             end
 
-            it { should have_selector('title', text: user.name) }
-            it { should have_link('Profile', href: user_path(user)) }
+
+            it { should have_link('Settings', href: settings_path) }
             it { should have_link('Sign out', href: signout_path) }
-            it { should_not have_link('Sign in', href: signin_path) }
         end
     end
 end
