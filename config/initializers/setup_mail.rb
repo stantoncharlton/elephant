4381acfc134475a26a4676b519b0bd0efacf75a7
Ryan Dawson <ryan@thirteen23.com>
2012-10-18 16:12:35 -0500

    Potential fix for new user email:

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -2,12 +2,12 @@ ActionMailer::Base.smtp_settings = {
         :address              => "smtp.gmail.com",
         :port                 => 587,
         :domain               => "go-elephant.herokuapp.com",
-        :user_name            => "ryan.dawson.thirteen23",
-        :password             => "kh6b3dqa!",
+        :user_name            => "ryan.dawson.thirteen23@gmail.com",
+        :password             => "elephant!3",
         :authentication       => "plain",
         :enable_starttls_auto => true
 }
 
-ActionMailer::Base.default_url_options[:host] = "heroku.com"
+ActionMailer::Base.default_url_options[:host] = "heroku.com" if Rails.env.production?
 ActionMailer::Base.default_url_options[:host] = "localhost:3000" if Rails.env.development?
 #Mail.register_interceptor(DevelopmentMailInterceptor) if Rails.env.development?
