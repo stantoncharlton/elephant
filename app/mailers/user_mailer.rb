487fbb723c439c7d63105459d376d89aacc823da
Ryan Dawson <ryan@thirteen23.com>
2012-12-13 11:39:37 -0600

    Updated email used for ryan

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,14 +3,14 @@ class UserMailer < ActionMailer::Base
 
   def registration_confirmation(user)
       @user = user
-      mail(:to => "ryan@thirteen23.com, mdawson@dawsonparrish.com, schuldes2004@gmail.com",
+      mail(:to => "ryan.dawson@go-elephant.com, mdawson@dawsonparrish.com, schuldes2004@gmail.com",
            :subject => "#{@user.company.name} added you to Elephant")
   end
 
   def reset_password(user, password)
       @user = user
       @password = password
-      mail(:to => "ryan@thirteen23.com, mdawson@dawsonparrish.com, schuldes2004@gmail.com",
+      mail(:to => "ryan.dawson@go-elephant.com, mdawson@dawsonparrish.com, schuldes2004@gmail.com",
            :subject => "Your Elephant new password")
   end
 end
