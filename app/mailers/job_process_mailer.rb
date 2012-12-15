78777365787217d9012184f4901e2fa0bc3d199b
Ryan Dawson <ryan@thirteen23.com>
2012-12-15 11:31:54 -0600

    Added text ellipsis to job data

--- app/mailers/job_process_mailer.rb
+++ app/mailers/job_process_mailer.rb
@@ -4,28 +4,28 @@ class JobProcessMailer < ActionMailer::Base
     def pre_job_data_complete(user, job)
         @user = user
         @job = job
-        mail(:to => "ryan.dawson@go-elephant.com, mdawson@dawsonparrish.com, schuldes2004@gmail.com",
+        mail(:to => "ryan.dawson@go-elephant.com, michael.dawson@go-elephant.com, roberto.schuldes@go-elephant.com",
              :subject => "Pre-Job complete: #{@job.field.name} | #{@job.well.name} | #{@job.job_template.product_line.name}")
     end
 
     def post_job_data_complete(user, job)
         @user = user
         @job = job
-        mail(:to => "ryan.dawson@go-elephant.com, mdawson@dawsonparrish.com, schuldes2004@gmail.com",
+        mail(:to => "ryan.dawson@go-elephant.com, michael.dawson@go-elephant.com, roberto.schuldes@go-elephant.com",
              :subject => "Post-Job complete: #{@job.field.name} | #{@job.well.name} | #{@job.job_template.product_line.name}")
     end
 
     def ship_to_field(user, job)
         @user = user
         @job = job
-        mail(:to => "ryan.dawson@go-elephant.com, mdawson@dawsonparrish.com, schuldes2004@gmail.com",
+        mail(:to => "ryan.dawson@go-elephant.com, michael.dawson@go-elephant.com, roberto.schuldes@go-elephant.com",
              :subject => "Job Shipping to Field: #{@job.field.name} | #{@job.well.name} | #{@job.job_template.product_line.name}")
     end
 
     def job_complete(user, job)
         @user = user
         @job = job
-        mail(:to => "ryan.dawson@go-elephant.com, mdawson@dawsonparrish.com, schuldes2004@gmail.com",
+        mail(:to => "ryan.dawson@go-elephant.com, michael.dawson@go-elephant.com, roberto.schuldes@go-elephant.com",
              :subject => "Job complete: #{@job.field.name} | #{@job.well.name} | #{@job.job_template.product_line.name}")
     end
 end
