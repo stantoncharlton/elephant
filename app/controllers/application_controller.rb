1f9f810afe2e7c0eefa4fd8da8b548779c86f749
Ryan Dawson <ryan@thirteen23.com>
2012-12-17 10:08:29 -0600

    Time zone test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,16 +2,13 @@ class ApplicationController < ActionController::Base
     protect_from_forgery
     include SessionsHelper
 
+    before_filter :set_user_time_zone
     before_filter :set_current_user_for_observer
     #before_filter :set_time_zone
 
     def set_current_user_for_observer
         puts request.path
         UserObserver.current_user = current_user
-    end
-
-    def set_time_zone
-        Time.zone = "Central Time (US & Canada)"
     end
 
 
@@ -32,4 +29,11 @@ class ApplicationController < ActionController::Base
     def not_found
         raise ActionController::RoutingError.new('Not Found')
     end
+
+private
+
+    def set_user_time_zone
+        Time.zone = "Central Time (US & Canada)" if logged_in?
+    end
+
 end
