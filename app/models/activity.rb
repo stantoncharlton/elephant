9d78842ad8624accf50bdc6415061cf0e888fe3b
Ryan Dawson <ryan@thirteen23.com>
2013-02-01 14:44:27 -0600

    Another postgres fix

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -128,7 +128,7 @@ class Activity < ActiveRecord::Base
     end
 
     def self.activities_for_jobs(jobs)
-        select("DISTINCT ON (activities.job_id) *").
+        select("DISTINCT ON (activities.job_id) activities.created_at, *").
                 where("job_id IN (?)", jobs.map { |j| j.id }).
                 where("activity_type >= :start_range AND activity_type <= :end_range", start_range: 100, end_range: 200).
                 order("created_at DESC").
