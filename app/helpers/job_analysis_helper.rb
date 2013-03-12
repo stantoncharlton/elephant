9fb4f902aeb80728d23b6940ba3045567a7769eb
Ryan Dawson <ryan@thirteen23.com>
2013-03-11 19:59:54 -0600

    Modified debug output

--- app/helpers/job_analysis_helper.rb
+++ app/helpers/job_analysis_helper.rb
@@ -3,6 +3,19 @@ module JobAnalysisHelper
     def personnel_utilization(jobs)
 
         all_users = User.where("users.district_id IN (?)", jobs.map { |j| j.district_id }.uniq).
+                where("users.product_line_id IN (?)", jobs.map { |j| j.job_template.product_line_id }.uniq)
+        active_users = JobMembership.where("job_memberships.job_id IN (?)", jobs.map { |j| j.id }.uniq)
+
+        active_users.map { |u| u.user_id }.uniq.each do |user_id|
+            puts User.find_by_id(user_id).name
+        end
+        puts "....................."
+        all_users.each do |user_id|
+           puts User.find_by_id(user_id).name
+        end
+
+
+        all_users = User.where("users.district_id IN (?)", jobs.map { |j| j.district_id }.uniq).
                 where("users.product_line_id IN (?)", jobs.map { |j| j.job_template.product_line_id }.uniq).count(:id)
         active_users = JobMembership.where("job_memberships.job_id IN (?)", jobs.map { |j| j.id }.uniq).group(:user_id).count(:user_id)
 
