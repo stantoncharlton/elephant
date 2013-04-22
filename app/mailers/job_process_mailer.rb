4bb6ca1488da61978f5f7cda428bed679b8dba00
Ryan Dawson <ryan@thirteen23.com>
2013-04-21 20:10:19 -0500

    Added new test email

--- app/mailers/job_process_mailer.rb
+++ app/mailers/job_process_mailer.rb
@@ -4,42 +4,42 @@ class JobProcessMailer < ActionMailer::Base
     def pre_job_data_complete(user, job)
         @user = user
         @job = job
-        mail(:to => user.company.test_company ? "test@go-elephant.com" :user.email,
+        mail(:to => user.company.test_company ? "test-emails@go-elephant.com" :user.email,
              :subject => "Pre-Job Complete: #{@job.field.name} | #{@job.well.name} | #{@job.job_template.name}")
     end
 
     def post_job_data_complete(user, job)
         @user = user
         @job = job
-        mail(:to => user.company.test_company ? "test@go-elephant.com" :user.email,
+        mail(:to => user.company.test_company ? "test-emails@go-elephant.com" :user.email,
              :subject => "Post-Job Complete: #{@job.field.name} | #{@job.well.name} | #{@job.job_template.name}")
     end
 
     def ship_to_field(user, job)
         @user = user
         @job = job
-        mail(:to => user.company.test_company ? "test@go-elephant.com" :user.email,
+        mail(:to => user.company.test_company ? "test-emails@go-elephant.com" :user.email,
              :subject => "Job Shipping to Field: #{@job.field.name} | #{@job.well.name} | #{@job.job_template.name}")
     end
 
     def job_complete(user, job)
         @user = user
         @job = job
-        mail(:to => user.company.test_company ? "test@go-elephant.com" :user.email,
+        mail(:to => user.company.test_company ? "test-emails@go-elephant.com" :user.email,
              :subject => "Job Complete: #{@job.field.name} | #{@job.well.name} | #{@job.job_template.name}")
     end
 
     def added_to_job(user, job)
         @user = user
         @job = job
-        mail(:to => user.company.test_company ? "test@go-elephant.com" :user.email,
+        mail(:to => user.company.test_company ? "test-emails@go-elephant.com" :user.email,
              :subject => "Added to Job: #{@job.field.name} | #{@job.well.name} | #{@job.job_template.name}")
     end
 
     def job_inactive(user, job)
         @user = user
         @job = job
-        mail(:to => user.company.test_company ? "test@go-elephant.com" :user.email,
+        mail(:to => user.company.test_company ? "test-emails@go-elephant.com" :user.email,
              :subject => "Job Low Activity Alert")
     end
 end
