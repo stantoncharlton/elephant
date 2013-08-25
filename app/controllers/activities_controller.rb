4808ebbfb949238ea648b33089386c37f58aef9a
Ryan Dawson <ryan@thirteen23.com>
2013-08-25 14:36:07 +0100

    Added user activity export

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -21,16 +21,22 @@ class ActivitiesController < ApplicationController
     def show
         respond_to do |format|
             format.xlsx do
-                puts "Start........."
-
                 if params[:job_activity] == "true"
                     @job = Job.find_by_id(params[:id])
                     not_found unless @job.company == current_user.company
 
                     @activities = Activity.activities_for_job(@job)
 
-                    excel = to_excel(@activities, @job)
-                    send_data excel.to_stream.read, :filename => 'activity.xlsx', :type => "application/vnd.openxmlformates-officedocument.spreadsheetml.sheet"
+                    excel = job_to_excel(@activities, @job)
+                    send_data excel.to_stream.read, :filename => "#{@job.id.to_s} Job Activity.xlsx", :type => "application/vnd.openxmlformates-officedocument.spreadsheetml.sheet"
+                elsif params[:user_activity] == "true"
+                    @user = User.find_by_id(params[:id])
+                    not_found unless @user.company == current_user.company
+
+                    @activities = Activity.activities_for_user(@user)
+
+                    excel = user_to_excel(@activities, @user)
+                    send_data excel.to_stream.read, :filename => "#{@user.name} Activity.xlsx", :type => "application/vnd.openxmlformates-officedocument.spreadsheetml.sheet"
                 end
             end
         end
--- app/helpers/activity_export_helper.rb
+++ app/helpers/activity_export_helper.rb
@@ -2,7 +2,7 @@ module ActivityExportHelper
 
     require 'axlsx'
 
-    def to_excel activities, job
+    def job_to_excel activities, job
         p = Axlsx::Package.new
         wb = p.workbook
 
@@ -14,6 +14,25 @@ module ActivityExportHelper
                     sheet.add_row [activity.user_name, activity.message, activity.target.text, activity.created_at.strftime("%a %m/%d/%Y %l:%M %p")]
                 else
                     sheet.add_row [activity.user_name, activity.message, activity.metadata, activity.created_at.strftime("%a %m/%d/%Y %l:%M %p")]
+                end
+            end
+        end
+
+        p
+    end
+
+    def user_to_excel activities, user
+        p = Axlsx::Package.new
+        wb = p.workbook
+
+        wb.add_worksheet(:name => "User - " + user.name + " (" + user.id.to_s + ")") do |sheet|
+            sheet.add_row ['Job', 'Action', 'Data', 'Date']
+
+            activities.each do |activity|
+                if activity.target.instance_of?(JobNote)
+                    sheet.add_row [activity.job_id.to_s + (activity.job.present? ?  " - " + activity.job.field.name + " | " + activity.job.well.name : ""), activity.message, activity.target.text, activity.created_at.strftime("%a %m/%d/%Y %l:%M %p")]
+                else
+                    sheet.add_row [activity.job_id.to_s + (activity.job.present? ?  " - " + activity.job.field.name + " | " + activity.job.well.name : ""), activity.message, activity.metadata, activity.created_at.strftime("%a %m/%d/%Y %l:%M %p")]
                 end
             end
         end
