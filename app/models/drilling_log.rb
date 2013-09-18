5b89d4ccd15316e8ff3c8ea0cfc57ae311f75a6f
Ryan Dawson <ryan@thirteen23.com>
2013-09-17 23:16:13 -0500

    Added drilling log entry

--- app/models/drilling_log.rb
+++ app/models/drilling_log.rb
@@ -3,12 +3,11 @@ class DrillingLog < ActiveRecord::Base
                     :entry_at,
                     :activity_code,
                     :depth,
-                    :usage_hours,
                     :user_name
 
     acts_as_tenant(:company)
 
-    validates :comment, presence: true, length: {minimum: 1, maximum: 500}
+    validates :comment, length: {minimum: 0, maximum: 500}
     validates :entry_at, presence: true
     validates_presence_of :company_id
     validates_presence_of :job_id
@@ -22,5 +21,30 @@ class DrillingLog < ActiveRecord::Base
     belongs_to :job
     belongs_to :bha
 
+
+    START = 1
+    CIRCULATING = 2
+    DRILLING = 3
+    CONNECTION = 4
+    SURVEY = 5
+    SLIDING = 6
+
+
+    def activity_code_string
+        case self.activity_code
+            when START
+                "Start"
+            when CIRCULATING
+                "Circulating"
+            when DRILLING
+                "Drilling"
+            when CONNECTION
+                "Connection"
+            when SURVEY
+                "Survey"
+            when SLIDING
+                "Sliding"
+        end
+    end
 
 end
