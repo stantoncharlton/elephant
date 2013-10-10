fd0820aaf1cc340645d40ee46e652a8304e2aa6a
Ryan Dawson <ryan@thirteen23.com>
2013-10-10 14:22:36 -0500

    Fixed warehouse problems

--- app/controllers/parts_controller.rb
+++ app/controllers/parts_controller.rb
@@ -30,14 +30,14 @@ class PartsController < ApplicationController
 
                 if @parts.empty?
                     @parts << Part.new
-                    render json: @parts.map { |part| {:value => "No asset found or available...", :name => "", :id => -1, :district_serial_number => -1} }
+                    render json: @parts.map { |part| {:value => "No asset found or available...", :name => "", :id => -1, :district_serial_number => -1, :warehouseL => ""} }
                     return
                 end
 
                 if params[:q].present?
-                    render json: @parts.map { |part| {:name => part.master_part.present? ? part.master_part.name : part.name, :id => part.id, :district_serial_number => part.district_serial_number, :serial_number => part.serial_number, material_number: part.material_number} }
+                    render json: @parts.map { |part| {:name => part.master_part.present? ? part.master_part.name : part.name, :id => part.id, :district_serial_number => part.district_serial_number, :serial_number => part.serial_number, material_number: part.material_number, warehouse: part.warehouse.name} }
                 else
-                    render json: @parts.map { |part| {:label => part.master_part.present? ? part.master_part.name : part.name, :id => part.id, :district_serial_number => part.district_serial_number, :serial_number => part.serial_number, material_number: part.material_number} }
+                    render json: @parts.map { |part| {:label => part.master_part.present? ? part.master_part.name : part.name, :id => part.id, :district_serial_number => part.district_serial_number, :serial_number => part.serial_number, material_number: part.material_number, warehouse: part.warehouse.name} }
                 end
             }
         end
