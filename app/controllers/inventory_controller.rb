aded1ce1a4402013720486db9078e099b42a65f7
Ryan Dawson <ryan@thirteen23.com>
2014-01-15 10:57:29 -0600

    Added new inventory view

--- app/controllers/inventory_controller.rb
+++ app/controllers/inventory_controller.rb
@@ -18,33 +18,8 @@ class InventoryController < ApplicationController
             @district = nil
         end
 
-
-        if @district.present?
-            if current_user.role.district_read? && !@district_present
-                @jobs = Job.where("jobs.district_id IN (SELECT id FROM districts where master_district_id = :district_id)", district_id: @district.id).where("jobs.status > ? AND jobs.status < 100", Job::ON_JOB).order("jobs.start_date DESC")
-            else
-                if current_user.role.limit_to_assigned_jobs?
-                    @jobs = Job.where("jobs.id in (select job_id from job_memberships where user_id = :user_id)", user_id: current_user.id).where("jobs.status > ? AND jobs.status < 100", Job::ON_JOB).order("jobs.start_date DESC")
-                else
-                    @jobs = Job.where("jobs.district_id = ?", @district.id).where("jobs.status > ? AND jobs.status < 100", Job::ON_JOB).order("jobs.start_date DESC")
-                end
-            end
-        elsif !current_user.role.limit_to_assigned_jobs?
-            @jobs = Job.where("jobs.status > ? AND jobs.status < 100", Job::ON_JOB).order("jobs.start_date DESC")
-        end
-
-        if !@district.nil?
-            if current_user.role.district_read?
-                @parts = Part.includes(:parts).where("parts.district_id = :district_id", district_id: @district.id).where(:template => false).order("parts.name ASC")
-            elsif current_user.role.limit_to_assigned_jobs?
-                @parts = Part.includes(:parts).where("parts.district_id = :district_id", district_id: @district.id).where(:template => false).order("parts.name ASC")
-            else
-                @parts = Part.includes(:parts).where("parts.district_id = :district_id", district_id: @district.id).where(:template => false).order("parts.name ASC")
-            end
-        elsif !current_user.role.limit_to_assigned_jobs?
-            @parts = Part.includes(:parts).where(:template => false).order("parts.name ASC")
-        end
-
+        not_found unless @district.present?
+        redirect_to inventory_path(@district)
     end
 
 
@@ -55,15 +30,18 @@ class InventoryController < ApplicationController
         @condensed = true
 
         if current_user.role.district_read?
-            @parts = Part.includes(:parts).where("parts.district_id = :district_id", district_id: @district.id).where(:template => true).order("parts.name ASC").paginate(page: params[:page], limit: 40)
+            @parts = Part.includes(:parts).where("parts.district_id = :district_id", district_id: @district.id).where(:template => false).order("parts.name ASC")
         elsif current_user.role.limit_to_assigned_jobs?
-            @parts = Part.includes(:parts).where("parts.district_id = :district_id", district_id: @district.id).where(:template => true).order("parts.name ASC").paginate(page: params[:page], limit: 40)
+            @parts = Part.includes(:parts).where("parts.district_id = :district_id", district_id: @district.id).where(:template => false).order("parts.name ASC")
         else
-            @parts = Part.includes(:parts).where("parts.district_id = :district_id", district_id: @district.id).where(:template => true).order("parts.name ASC").paginate(page: params[:page], limit: 40)
+            @parts = Part.includes(:parts).where("parts.district_id = :district_id", district_id: @district.id).where(:template => false).order("parts.name ASC")
         end
 
         respond_to do |format|
             format.html {
+
+            }
+            format.js {
 
             }
             format.xlsx {
