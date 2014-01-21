70d04aafa907d9932ffbc6aaa3ba3072ce34829c
Ryan Dawson <ryan@thirteen23.com>
2014-01-21 09:41:42 -0600

    Fixed survey tie-in edit, allowing 0 for MD

--- app/controllers/survey_points_controller.rb
+++ app/controllers/survey_points_controller.rb
@@ -77,7 +77,7 @@ class SurveyPointsController < ApplicationController
         @survey_point = SurveyPoint.find_by_id(params[:id])
         not_found unless @survey_point.present?
         if params[:survey_point][:measured_depth].present? &&
-                params[:survey_point][:measured_depth].to_f != 0 &&
+                (params[:survey_point][:measured_depth].to_f != 0 || @survey_point.tie_on?) &&
                 params[:survey_point][:inclination].present? &&
                 params[:survey_point][:azimuth].present?
             @survey_point.update_attributes(params[:survey_point])
