d334dd79e621debca52eb3b991be2db373ca450c
Ryan Dawson <ryan@thirteen23.com>
2014-01-21 20:37:25 -0600

    Fix for new survey and changing drilling log

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -129,7 +129,7 @@ class SurveysController < ApplicationController
                 end
 
                 if @survey.survey_points.any?
-                    drilling_log = DrillingLog.includes(job: :well).where("wells.id = ?", job.well_id).first
+                    drilling_log = DrillingLog.includes(job: :well).where("wells.id = ?", @survey.document.job.well_id).first
                     if drilling_log.present?
                         drilling_log.update_attribute(:td_depth, @survey.survey_points.last.measured_depth)
                     end
